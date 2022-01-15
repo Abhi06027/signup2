bb9640a828409287ae9cc104100a823fc0b705a6
Abhi06027 <abhinavk67@gmail.com>
2022-01-15 21:52:58 +0530

    sign up

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <h1 className="text-3xl font-bold bg-red-700 text-white underline">
+      Hello world!
+    </h1>
+    </div>
+  );
+}
+
+export default App;
